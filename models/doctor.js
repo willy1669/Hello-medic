42b14c706de9b64c55a29707ee05e97e97c62e28
willy1669 <williamschinonso11@gmail.com>
2019-04-15 18:10:47 +0100

    set email to a unique value

--- models/doctor.js
+++ models/doctor.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const doctorSchema = mongoose.Schema({
     firstname: String,
     lastname: String,
-    email: String,
+    email: { type: String, unique: true},
     password: String,
     specialty: String,
     hospitalName: String,
