a1e63246a1a39554e42cb7412aa7d4fcbc10b95c
willy1669 <williamschinonso11@gmail.com>
2019-04-21 15:09:59 +0100

    add getDoctors route

--- router/doctorRoute.js
+++ router/doctorRoute.js
@@ -5,5 +5,7 @@ const doctorController = require('../controllers/doctorController');
 //GET graduate listing.
 router.post('/signUp', doctorController.doctorSignUp);
 router.post('/login', doctorController.loginUser);
+router.get('/', doctorController.getDoctors);
+//router.post('addProfile', doctorController.addProfile);
 
 module.exports = router;
