7c21cfa59a332008c67b517bb7964b22b8f4d094
willy1669 <williamschinonso11@gmail.com>
2019-05-16 11:51:16 +0100

    create baseRepository to handle mongoose queries

--- repositories/baseRepository.js
+++ repositories/baseRepository.js
@@ -7,6 +7,20 @@ BaseRepository.prototype.add = function (data, callback) {
     this.model.create(data, callback);
 }
 
-module.exports = function(model){
+BaseRepository.prototype.getAll = (options, columns, callback) => {
+    console.log("model", this.model)
+    this.model.find(options, columns, callback);
+    
+}
+
+BaseRepository.prototype.getById = function(id, callback){
+    this.model.findById(id, callback);
+}
+
+BaseRepository.prototype.update= (id, options, callback) => {
+    this.model.findByIdAndUpdate(id, options, callback);
+}
+
+module.exports = (model) =>{
     return new BaseRepository(model);
 }
