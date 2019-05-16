1ebecf22612c752580b4ecea9d8e182b60e7d93d
willy1669 <williamschinonso11@gmail.com>
2019-05-16 11:55:21 +0100

    modified doctorLogin route

--- router/doctorRoute.js
+++ router/doctorRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const doctorController = require('../controllers/doctorController');
+const authMiddleware = require('../middleware/temporaryToken');
 
 //GET graduate listing.
 router.post('/signUp', doctorController.doctorSignUp);
-router.post('/login', doctorController.loginUser);
+router.post('/login', authMiddleware.validateToken, doctorController.loginUser);
 router.get('/', doctorController.getDoctors);
-//router.post('addProfile', doctorController.addProfile);
+//router.get('/search', doctorController.searchDoctors)
+router.post('/createProfile', doctorController.createProfile);
 
 module.exports = router;
