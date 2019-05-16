e09cb6cdfcb5d6f7c499e68b18c94e649e35b64a
willy1669 <williamschinonso11@gmail.com>
2019-05-16 08:03:18 +0100

    modified doctor signUp function

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -5,6 +5,7 @@ const service = require('../services/doctorService');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const config = require('../config.js')
 
 //Define schema for validating user input
 const schema = joi.object().keys({
@@ -21,9 +22,9 @@ exports.doctorSignUp = (req, res) => {
         email: req.body.email,
         password: req.body.password,
     }
-    // validating the employer input
+    // validating the doctor input
     joi.validate({firstname: data.firstname, lastname: data.lastname, email: data.email, password: data.password}, schema, function (err) {
-        try{
+        try {
             if (err) {
                 res.json({err: err.message});
             } 
@@ -40,17 +41,17 @@ exports.doctorSignUp = (req, res) => {
     })
 }
 
-function isValidPassword(user, password){
-    return passwordHash.verify(password, user.password);
+function isValidPassword(doctor, password){
+    return passwordHash.verify(password, doctor.password);
 }
 
-passport.serializeUser(function(user, done){
-    done(null, user.id)
+passport.serializeUser(function(doctor, done){
+    done(null, doctor.id)
 });
 
 passport.deserializeUser(function(id, done) {
-    model.findById(id, function(err, user) {
-        done(err, user);
+    model.findById(id, function(err, doctor) {
+        done(err, doctor);
     });
 });
 
@@ -61,21 +62,27 @@ exports.loginUser = function (req, res) {
     });
     try {
         passport.use ('login', new LocalStrategy(
-            model.findOne({email: req.body.email}, function (err, user) {
+            model.findOne({email: req.body.email}, function (err, doctor) {
                 if (err) {
                     res.json({err: err});
                 }
-                if (user && isValidPassword(user, req.body.password)) {
-                    var token = jwt.sign({email: user.email, id: user._id}, '+secret+', {expiresIn: '12h'});
-                    res.json({userId:user._id, email:user.email, username: user.username, token: token, message: 'Login successful.'});
+                if (doctor && isValidPassword(doctor, req.body.password)) {
+                    var token = jwt.sign({email: doctor.email, id: doctor._id, temporaryToken: req.cartTemporaryId}, config.secret, {expiresIn: '12h'});
+                    console.log("token", jwt.decode(token, config.secret))
+                    res.json({docotrId:doctor._id, email:doctor.email, token: token, message: 'Login successful.'});
                 }
                 else {
                     res.json({message: 'Incorrect email or password.'});
-                    console.log(isValidPassword (req.body.password));
+                    //console.log(isValidPassword (req.body.password));
                 }
-                console.log(user);
+                console.log(doctor);
             }),
-        ));
+        ), function(err) {
+            if (err) res.json({err:err, message: "create an account"})
+            if (doctor == null) {
+
+            }
+        });
     }
     catch (exception) {
         console.log(exception);
@@ -99,11 +106,24 @@ exports.getDoctors = function (req, res,){
     }
 }
 
-exports.bookAnAppointment = (req, res) => {
-    var data = {
-        user: req.body.user,
+exports.createProfile = (req, res) => {
+    var id = req.body.id;
+    var profile = {
+        specialization : req.body.specialization,
+        hospitalName : req.body.hospitalName,
+        age : req.body.age,
+        littleBiography : req.body.littleBiography,
+        location : req.body.location, 
+        consultancyFee : req.body.consultancyFee
+    }
+    try {
+        return service.createProfile(req, res, id, profile)
+    }    
+    catch(exception) {
+        console.log("Error: "+exception)
     }
 }
+
 
 // exports.addProfile = (req, res) => {
 //     data = {
@@ -121,6 +141,18 @@ exports.bookAnAppointment = (req, res) => {
 //         console.log(exception);
 //     }
 // }
+
+exports.getDoctorsByAppointment = (req, res) => {
+    appointment = req.body.appointment;
+    doctor = req.body.doctor
+    try {
+        return service.getDoctorsByAppointment(appointment, doctor)
+    }
+    catch (exception) {
+        console.log("Error: "+exception)
+    }
+}
+
 
 
 
