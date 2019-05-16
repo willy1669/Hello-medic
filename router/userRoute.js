a7358614b4c5a2bf93473797b6bd142447f83d28
willy1669 <williamschinonso11@gmail.com>
2019-05-16 12:00:49 +0100

    create loginUser route

--- router/userRoute.js
+++ router/userRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/temporaryToken');
+
 
 //GET graduate listing.
 router.post('/signUp', userController.userSignUp);
-router.post('/login', userController.loginUser);
-// router.post('/appointment', userController.bookAnAppointment);
+router.post('/login', authMiddleware.validateToken, userController.loginUser);
+router.get('/', userController.getAllUsers);
+router.get('/logout', userController.logOutUser)
 
 module.exports = router;
