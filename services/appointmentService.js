646e0be7c2e3c39a5d02de4151f91f732baa4c3c
willy1669 <williamschinonso11@gmail.com>
2019-05-24 13:45:30 +0100

    modified bookAppointment function

--- services/appointmentService.js
+++ services/appointmentService.js
@@ -1,81 +1,94 @@
-//const repository = require('../repository/appointmentRepository');
 const model = require('../models/appointment');
 const docModel = require('../models/doctor')
 const userModel = require('../models/user');
 
-// exports.bookAppointment = (req, res, doctor, appointmentDate, appointmentStartTime, appointmentEndTime, user) => {
-// 	docModel.findOne({"_id": doctor}).exec((err, doctorData) => {
-// 		console.log("doctorData", doctorData)
+// exports.bookAppointment = (req, res, doctor, user, appointmentDate, appointmentTime, data) => {
+// 	docModel.findOne({"_id": doctor}).exec((err, doctordata) => {
+// 		console.log("doctor", doctordata)
 // 		if (err) {
 // 			res.json({err: err, message: "baba, u no dey database"})
 // 		}
 // 		else {
-// 			if (doctorData) {
-// 				userModel.findOne({"userId": user}).exec((err, result) => {
-// 					console.log("response", user)
+// 			if (doctordata) {
+// 				userModel.findOne({"_id": user}).exec((err) => {
 // 					if (err) {
-// 						res.json({err: err})
-// 					}
-// 					else if (result) {
-// 						res.json({message:'You already have an appointment scheduled for this day. Please select a different day.'})
+// 						res.json({message: 'guy, biko try go sign up' })
 // 					}
 // 					else {
-// 						model.find({"userId": user}, function(err, result) {
-// 							if(err) {
+// 						model.findOne(appointmentDate).exec((err, response) => {
+// 							if (err) {
 // 								return res.status(402).json(err)
 // 							}
-// 							else if(result > 0){
-// 								return res.status(403).json('This appointment time is unavailable. Appointments must be scheduled at least 20 minutes apart. Please choose a different time.')
+// 							else if(response.length > 2){
+// 								return res.status(403).json('Doctor already has three appointments this day. Please select a different day.')
 // 							}
-// 							else{
-// 								let appointment = new model(req.body);
-// 								appointment.data = data;
-// 								appointment.appointmentDate = appointmentDate;
-// 								appointment.appointmentStartTime = appointmentStartTime;
-// 								// appointmnet.appointmentEndTime = appointmentEndTime;
-// 								appointment.save( err => {
+// 							else {
+// 								model.find({"_id": user, 'appointmentDate': appointmentDate}, function(err, response) {
 // 									if(err) {
-// 										console.log(err);
-// 										return res.status(402).json(err);
+// 										return res.status(402).json(err)
+// 									}
+// 									else if(response.length > 0){
+// 										return res.status(403).json('You already have an appointment scheduled for this day. Please select a different day.')
 // 									}
 // 									else{
-// 										console.log(appointment);
-// 										return res.json(appointment)
+// 										Appointment.find({'appointmentDate': appointmentDate, 'appointmentTime': {$gt: req.body.time-20, $lt: req.body.time+20}}, function(err, response) {
+// 											if(err) {
+// 												return res.status(402).json(err)
+// 											}
+// 											else if(response.length > 0){
+// 												return res.status(403).json('This appointment time is unavailable. Appointments must be scheduled at least 20 minutes apart. Please choose a different time.')
+// 											}
+// 											else{
+// 												var appointment = new model ();
+// 												appointment.data = data;
+// 												appointment.appointmentDate = appointmentDate;
+// 												appointment.appointmentTime = appointmentTime;
+// 												appointment.save( err => {
+// 													if(err) {
+// 														console.log(err);
+// 														return res.status(402).json(err);
+// 													}
+// 													else{
+// 														console.log(appointment);
+// 														return res.json(appointment)
+// 													}
+// 												})
+// 											}
+// 										})
 // 									}
 // 								})
 // 							}
-
 // 						})
 // 					}
 // 				})
 // 			}
 // 		}
 // 	})
-// }
+// }	
 
-exports.bookAppointment = (req, res, doctor, user, appointmentDate, data) => {
-	docModel.findOne({"_id": doctor}).exec((err, doctordata) => {
-		console.log("doctor", doctordata)
+
+exports.bookAppointment = (req, res, doctor, user, appointmentDate, appointmentTime) => {
+	model.findOne(docotr).exec((err, docResult) => {
 		if (err) {
-			res.json({err: err, message: "baba, u no dey database"})
+			res.json({err: err})
 		}
 		else {
-			if (doctordata) {
-				userModel.findOne({"_id": user}).exec((err) => {
+			if (doctor) {
+				model.findOne(appointmentDate).exec((err, result) => {
 					if (err) {
-						res.json({message: 'guy, biko try go sign up' })
+						res.json({message: 'could not confirm date'})
 					}
 					else {
-						model.findOne(appointmentDate).exec((err, result) => {
-							if 
-						})
+						if (result) {
+							
+						}
 					}
 				})
 			}
-			
 		}
 	})
 }
+
 
 exports.getAllAppointments = (req, res, options) => {
 	model.find(options, '-__v', function(err, appointments){
