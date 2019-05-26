ec4e5598a554e2aab56a5aae94df4f9c1d5ab483
willy1669 <williamschinonso11@gmail.com>
2019-05-26 13:57:32 +0100

    changed the searchDoctors route

--- router/doctorRoute.js
+++ router/doctorRoute.js
@@ -7,7 +7,7 @@ const authMiddleware = require('../middleware/temporaryToken');
 router.post('/signUp', doctorController.doctorSignUp);
 router.post('/login', authMiddleware.validateToken, doctorController.loginUser);
 router.get('/', doctorController.getDoctors);
-router.get('/search/', doctorController.searchDoctors)
+router.get('/search/:specialization', doctorController.searchDoctors)
 router.post('/createProfile', doctorController.createProfile);
 router.get('/id', doctorController.getDoctorById);
 
