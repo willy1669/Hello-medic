ddf6298bf508f6bd71f1f679175e43e627421007
willy1669 <williamschinonso11@gmail.com>
2019-05-27 14:09:15 +0100

    edited cloudinary module

--- cloudinary.js
+++ cloudinary.js
@@ -1,11 +1,13 @@
 var cloudinary = require('cloudinary');
-var keys = require('./config/keys').clodinary
+var keys = require('./config').cloudinary
 
-cloudinary.config({
-    cloud_name: 'nonso123',
-    api_key: '718354861464973',
-    api_secret: 'RceD8SSfVjqHCaK9kuoZyVsbNeE'
-});
+// cloudinary.config({
+//     cloud_name: 'nonso123',
+//     api_key: '718354861464973',
+//     api_secret: 'RceD8SSfVjqHCaK9kuoZyVsbNeE'
+// });
+
+
 
 exports.uploadToCloud = function(filename) {
     return new Promise(resolve => {
